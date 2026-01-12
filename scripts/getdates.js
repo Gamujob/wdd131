b0a554ea2859c71e63faf8fc213593005524dc00
gamujob <jgamutambuli@byupathway.edu>
2026-01-12 16:52:50 +0300

    Added features in the JS file

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,11 @@
+// This is for getting the current year
 const year = document.querySelector("#currentyear");
 const today = new Date();
 
-year.innerHTML = `©${today.getFullYear()}`
-document.getElementById("lastModified").innerHTML = `LastModified: ${document.lastModified}`;
+year.innerHTML = `©${today.getFullYear()} 🌹 Job Gamutambuli 🌹 Uganda`;
+
+// This is the one for the modified date
+const modified = document.getElementById("lastModified");
+modified.innerHTML = `LastModified: ${document.lastModified}`;
 
 
