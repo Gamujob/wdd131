727d0c13a7e880c9d711b79aa84dadb35a96ab5d
gamujob <jgamutambuli@byupathway.edu>
2026-02-01 22:04:21 +0300

    w02 BOM

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,28 +1,31 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('ul');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+const list = document.querySelector('#list');
+
 
 button.addEventListener('click', function() {
-  if (input.value.trim() === '') {
-    list.textContent = "Please input something.";
-    input.focus();
-    return;
-  }
-  
-})
+  if (input.value.trim() != '') {
+    const li = document.createElement('li');
+    li.textContent = input.value;
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
+    deleteButton.addEventListener('click', function() {
+      list.removeChild(li);
+      input.focus();
+    });
 
-deleteButton.addEventListener('click', function() {
-  list.removeChild(li);
+    li.appendChild(deleteButton);
+    list.appendChild(li);
+    input.value = '';
+  }
+
   input.focus();
-  input.value = '';
-  input.focus();
-})
+});
+
+// return;
+// list.textContent = "Please input something.";
+// input.focus();
+
 
 
 
