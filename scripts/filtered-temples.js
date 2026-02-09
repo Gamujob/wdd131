a16ac813429d4fc23bc94a64068727b58f56aa0d
gamujob <jgamutambuli@byupathway.edu>
2026-02-09 19:10:59 +0300

    Week four filtered temples

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,124 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation')
+hamButton.addEventListener('click', () => { 
+  navigation.classList.toggle('open');
+  hamButton.classList.toggle('open');
+});
+
+// This is for getting the current year
+const year = document.querySelector("#currentyear");
+const today = new Date();
+
+year.innerHTML = `©${today.getFullYear()} 💠 Job Gamutambuli 💠 Uganda`;
+
+// This is the one for the modified date
+const modified = document.getElementById("lastModified");
+modified.innerHTML = `LastModified: ${document.lastModified}`;
+
+const temples = [
+  {
+    templeName: "Aba Nigeria",
+    location: "Aba, Nigeria",
+    dedicated: "2005, August, 7",
+    area: 11500,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+  },
+  {
+    templeName: "Manti Utah",
+    location: "Manti, Utah, United States",
+    dedicated: "1888, May, 21",
+    area: 74792,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+  },
+  {
+    templeName: "Payson Utah",
+    location: "Payson, Utah, United States",
+    dedicated: "2015, June, 7",
+    area: 96630,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+  },
+  {
+    templeName: "Yigo Guam",
+    location: "Yigo, Guam",
+    dedicated: "2020, May, 2",
+    area: 6861,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+  },
+  {
+    templeName: "Washington D.C.",
+    location: "Kensington, Maryland, United States",
+    dedicated: "1974, November, 19",
+    area: 156558,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+  },
+  {
+    templeName: "Lima Perú",
+    location: "Lima, Perú",
+    dedicated: "1986, January, 10",
+    area: 9600,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+  },
+  {
+    templeName: "Mexico City Mexico",
+    location: "Mexico City, Mexico",
+    dedicated: "1983, December, 2",
+    area: 116642,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+  },
+  // Add more temple objects here...
+  {
+    templeName: "Nairobi Kenya",
+    location: "Nairobi, Kenya",
+    dedicated: "2025, May, 18",
+    area: 19870,
+    imageUrl: "https://www.churchofjesuschrist.org/imgs/xbrnlkgvoumomut7itk86rrpepzrej7pax1mvuzz/full/500%2C/0/default"
+  },
+  {
+    templeName: "Alabang Philippines",
+    location: "Philippines",
+    dedicated: "2026, January, 18",
+    area: 35998,
+    imageUrl: "https://www.churchofjesuschrist.org/imgs/2cr3d0ah18yg08jyhl9vxg8cte1ge96z7q4h8c98/full/500%2C/0/default"
+  },
+  {
+    templeName: "Brisbane Australia",
+    location: "Kangaroo Point, Queensland, Australia",
+    dedicated: "2003, June, 15",
+    area: 10700,
+    imageUrl: "https://www.churchofjesuschrist.org/imgs/28f8485c143afac0623e026608261d761be828d9/full/500%2C/0/default"
+  }
+];
+
+const templeList = document.getElementById('templeList');
+
+temples.forEach(temple => {
+  const div = document.createElement('div');
+  // div.classList.add('temple-card')
+
+  const name = document.createElement('p');
+  name.innerHTML = `<strong>${temple.templeName}</strong>`;
+
+  const location = document.createElement('p');
+  location.textContent = `Location: ${temple.dedicated}`;
+
+  const dedicated = document.createElement('p');
+  dedicated.textContent = `Dedicated: ${temple.dedicated}`;
+
+  const area = document.createElement('p');
+  area.textContent = `Area: ${temple.area}`;
+
+  const templeImage = document.createElement('img');
+  templeImage.src = temple.imageUrl;
+  templeImage.alt = temple.templeName;
+  templeImage.width = 300;
+
+  div.append(name, location, dedicated, area, templeImage);
+  templeList.appendChild(div);
+});
